3413d28c75b4edc13ac199461254256ea3334d02
omer9706 <omer_yurtseven1997@outlook.com>
2024-02-21 18:13:21 +0300

    Styles changed.

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -6,11 +6,14 @@ const HeroSection = () => {
     <section>
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-content-center text-center sm:text-left'>
-          <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Ankara Jeneratör</h1>
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Ankara {" "}Jeneratör</span>
+            Jeneratör
+          </h1>
           <p className='text-[#ADB7BE] text-base lg:text-xl'>Sektörde 20 yıldır hizmet veren Ankara Jeneratör, jeneratör kiralama, jeneratör satışı, jeneratör bakımı, jeneratör yedek parça ve jeneratör servisi konularında hizmet vermektedir.</p>
           <div className='m-5 flex gap-2 flex-col sm:flex-row items-center'>
-            <button className='bg-[#FFD700] text-[#181818] w-full px-8 py-3 rounded-full font-semibold text-lg lg:text-xl'>Hemen Ara</button>
-            <button className=' text-[#ADB7BE] px-8 py-3 w-full rounded-full font-semibold text-lg lg:text-xl border border-[#ADB7BE]'>Teklif Al</button>
+            <button className='  w-full sm:w-fit px-8 py-3 rounded-full font-semibold text-lg lg:text-xl bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-black'>Hemen Ara</button>
+            <button className=' text-white px-8 py-3 w-full sm:w-fit rounded-full  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-lg lg:text-xl'>Teklif Al</button>
           </div>
         </div>
         <div className='col-span-5'>
